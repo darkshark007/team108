0c547284c5cf154379b25784791964b77ceea9ea
darkshark007 <darkshark_007@yahoo.com>
2014-01-25 18:21:36 -0700

    NT Changes

--- Orders/NoiseTower.java
+++ Orders/NoiseTower.java
@@ -11,6 +11,7 @@ import battlecode.common.RobotType;
 public abstract class NoiseTower extends Orders {
 
 	final static int MAX_RANGE = RobotType.NOISETOWER.attackRadiusMaxSquared;
+	final static int MAX_RADIUS = (int)Math.sqrt(MAX_RANGE);
 	
 	public NoiseTower(RobotController in) { super(in); }
 	
@@ -28,6 +29,14 @@ public abstract class NoiseTower extends Orders {
 		sll.add(new MapLocation(start.x,start.y+1));
 		
 		return sll;
+	}
+	
+	protected int ntLocToIndex(MapLocation loc) {
+		return (35*(loc.x-(myLoc.x-MAX_RADIUS)))+(loc.y-(myLoc.y-17));
+	}
+
+	protected MapLocation indexToNTLoc(int index) {
+		return new MapLocation((int)(index/35)+(myLoc.x-17),(index % 35)+(myLoc.y-17));
 	}
 
 }
